a6a52465a1877222246ce89c5b3012b441c326dd
daner-coria <danereidance@gmail.com>
2026-02-24 12:49:57 +0100

    Primer commit - estructura inicial del meu projecte

---
+++ src/main/java/com/institut/cloudapi/DamCloudApiApplication.java
@@ -0,0 +1,13 @@
+package com.institut.cloudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamCloudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamCloudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institut/cloudapi/DamCloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institut.cloudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamCloudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
